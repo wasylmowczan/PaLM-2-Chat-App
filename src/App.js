adf589fb90fa5cbf062a9e5faa3d4de50d123237
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2023-11-17 21:04:17 +0100

    Basic app setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='chat-bot'>
+      <div className='chat-header'>
+        <div className='info-container'>
+          <h3>Chat with</h3>
+          <h2>PaLM 2 Bot</h2>
+        </div>
+        //svg
+      </div>
+      <div className='feed'>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
