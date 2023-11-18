cdfeab15fb63352a12dddbf8fd27c3e1f16d5105
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2023-11-18 21:20:37 +0100

    Add styles to elements

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import './App.css';
+import './App';
 
 const App = () => {
   return (
-    <div className='chat-bot'>
-      <div className='chat-header'>
-        <div className='info-container'>
+    <div className="chat-bot">
+      <div className="chat-header">
+        <div className="info-container">
           <h3>Chat with</h3>
           <h2>PaLM 2 Bot</h2>
         </div>
-        //svg
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="rgb(6, 120, 84)"
+            fillOpacity="1"
+            d="M0,224L60,218.7C120,213,240,203,360,186.7C480,171,600,149,720,154.7C840,160,960,192,1080,186.7C1200,181,1320,139,1380,117.3L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+          ></path>
+        </svg>
       </div>
-      <div className='feed'>
-
+      <div className="feed">
+        <div className="question bubble"></div>
+        <div className="response bubble"></div>
       </div>
-
+      <textarea />
+      <button>➡️</button>
     </div>
   );
 }
